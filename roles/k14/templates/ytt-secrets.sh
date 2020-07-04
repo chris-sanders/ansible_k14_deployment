30fc39fb5e448cfee8bef6323e04382028e835cf
Chris Sanders <sanders.chris@gmail.com>
2020-07-04 13:43:52 -0500

    Include default values in secrete processing, traefik skip auth if no users

--- roles/k14/templates/ytt-secrets.sh
+++ roles/k14/templates/ytt-secrets.sh
@@ -1,6 +1,8 @@
 {% for secret in k14_secrets_list["results"]: %}
     echo "{{ site_file_content }}" | \
-    ytt -f {{ secret.item }} -f - \
+    ytt -f {{ secret.item }} \
+    -f {{ ansible_parent_role_paths[0] }}/files/templates/{{ k14_default_values }} \
+    -f - \
     --ignore-unknown-comments \
     {% if k14_sops.found: %}
 	| sops --input-type yaml --output-type yaml -e /dev/stdin \

88184bb5d6c892bdc0409aabd3c26dfb15dbb027
Chris Sanders <sanders.chris@gmail.com>
2020-07-04 14:15:49 -0500

    Switch to k14 submodule

--- roles/k14/templates/ytt-templates.sh
+++
@@ -1,8 +0,0 @@
-echo "{{ site_file_content }}" | \
-ytt -f {{ ansible_parent_role_paths[0] }}/files/templates \
---ignore-unknown-comments \
---output-files {{ tmp_folder.path }}/ytt \
-{% if helm_overlay.stat.exists: %}
-    -f {{ helm_overlay.stat.path }} \
-{% endif %}
-    -f -

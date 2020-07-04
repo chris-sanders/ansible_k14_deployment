7fbfe89677e9e81af7465497b645d6ecf6501a74
Chris Sanders <sanders.chris@gmail.com>
2020-07-04 09:59:20 -0500

    Fix loading jinja-vars to include site overrides

--- roles/k14/templates/ytt-jinja-vars.sh
+++ roles/k14/templates/ytt-jinja-vars.sh
@@ -1,4 +1,5 @@
+echo "{{ site_file_content }}" | \
 ytt -f {{ role_path }}/files/jinja-vars.yaml \
--f {{ stat_site_file.stat.path }} \
 -f {{ ansible_parent_role_paths[0] }}/files/templates/{{ k14_default_values }} \
---output-files {{ tmp_folder.path }}
+--output-files {{ tmp_folder.path }} \
+-f -

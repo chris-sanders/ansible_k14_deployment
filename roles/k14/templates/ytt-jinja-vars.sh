5cf50294beecef5552feff79eee7d72cd578f704
Chris Sanders <sanders.chris@gmail.com>
2020-07-04 11:14:31 -0500

    Ignore unknown comments on ytt processing

--- roles/k14/templates/ytt-jinja-vars.sh
+++ roles/k14/templates/ytt-jinja-vars.sh
@@ -1,5 +1,6 @@
 echo "{{ site_file_content }}" | \
 ytt -f {{ role_path }}/files/jinja-vars.yaml \
+--ignore-unknown-comments \
 -f {{ ansible_parent_role_paths[0] }}/files/templates/{{ k14_default_values }} \
 --output-files {{ tmp_folder.path }} \
 -f -
--- roles/k14/templates/ytt-secrets.sh
+++ roles/k14/templates/ytt-secrets.sh
@@ -1,6 +1,7 @@
 {% for secret in k14_secrets_list["results"]: %}
     echo "{{ site_file_content }}" | \
     ytt -f {{ secret.item }} -f - \
+    --ignore-unknown-comments \
     {% if k14_sops.found: %}
 	| sops --input-type yaml --output-type yaml -e /dev/stdin \
     {% endif %}
